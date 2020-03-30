fdd3577231d7d372080a726584ffb294ab6fd175
EliveltonEGS <eliveltongsoficial@gmail.com>
2020-03-30 16:01:53 -0300

    servico de aplicacao aluno

---
+++ Pilates.Application/Services/Aluno/ApplicationServiceAluno.cs
@@ -0,0 +1,49 @@
+﻿
+using Pilates.Adapter.Map.CadastroBaseAluno;
+using Pilates.DTO.DTO;
+using Pilates.Service.Services.CadastroBase.CadastroBaseAluno;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Pilates.Application.Services.Aluno
+{
+    public class ApplicationServiceAluno : IApplicationServiceAluno
+    {
+        private readonly IServiceAluno _serviceAluno;
+        private readonly IMapperAluno _mapperAluno;
+
+        public ApplicationServiceAluno(
+            IServiceAluno serviceAluno,
+            IMapperAluno mapperAluno)
+        {
+            _serviceAluno = serviceAluno;
+            _mapperAluno = mapperAluno;
+        }
+
+        public void DeleteById(Guid id)
+        {
+            _serviceAluno.DeleteById(id);
+        }
+
+        public async Task<IEnumerable<AlunoDTO>> GetAll()
+        {
+            return await _mapperAluno.MapperListAlunos(_serviceAluno.GetAll());
+        }
+
+        public AlunoDTO GetById(Guid id)
+        {
+            return _mapperAluno.MapperToDTO(_serviceAluno.GetById(id));
+        }
+
+        public void Save(AlunoDTO input)
+        {
+            _serviceAluno.Save(_mapperAluno.MapperToEntity(input));
+        }
+
+        public void Update(AlunoDTO input)
+        {
+            _serviceAluno.Update(_mapperAluno.MapperToEntity(input));
+        }
+    }
+}
